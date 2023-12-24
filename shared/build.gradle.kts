76b8b19e5a1e5c3ebe5f4a8870204ea7f3ddf34d
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-12-24 14:44:37 -0300

    Adding kotlinx datetime library

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -36,7 +36,7 @@ kotlin {
     
     sourceSets {
         commonMain.dependencies {
-            //put your multiplatform dependencies here
+            implementation(libs.kotlinx.datetime)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
