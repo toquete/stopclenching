f090ac0b2a2e60208bb2a5d67b7e858772e8e36c
Guilherme Toquete <guilherme.toquete@gmail.com>
2023-12-28 18:10:57 -0300

    Removing sample files

--- shared/src/commonMain/kotlin/com/toquete/stopclenching/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.toquete.stopclenching
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
